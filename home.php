c5c1c6844ec5f05b94b3bfa2702e04cb45e4d7fa
Marco <marco@defrostudio.eu>
2020-06-30 16:19:58 +0200

    home template

---
+++ home.php
@@ -0,0 +1,19 @@
+<?php 
+	/* Template Name: Home */
+?>
+
+<?php get_header(); ?>
+
+<section>
+
+    <div>
+		<h1>Europe<br>Archive</h1>
+        <h6>Erik Kessels / Thomas Mailaender</h6>
+        <hr>
+    </div>
+
+</section>
+
+
+<?php
+get_footer();

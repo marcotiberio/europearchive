f1b11c74177bd467f4481dc9652a8e575b26af4e
Marco <marco@defrostudio.eu>
2020-07-02 15:39:41 +0200

    ejwhjleh

--- item.php
+++ item.php
@@ -14,13 +14,13 @@
         <div class="item-location"><h3>Belgium - 1990</h3></div>
         <div class="item-description">
             <div class="item-country-logo">
-                <img src="http://europearchive.eu/wp-content/uploads/2020/06/belgium-map-silhouette-vector-14558531.jpg" alt="">
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                <span><img src="http://europearchive.eu/wp-content/uploads/2020/06/belgium-map-silhouette-vector-14558531.jpg" alt=""></span>
+                <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
                 incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
                 exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
                 dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
                 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
-                anim id est laborum.</p>
+                anim id est laborum.</span>
             </div>
         </div>
     </div>

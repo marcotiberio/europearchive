340890088e8e89a12a33280c64be6ec38ae8c81a
Marco <marco@defrostudio.eu>
2020-07-02 15:29:59 +0200

    item template

--- item.php
+++ item.php
@@ -5,3 +5,28 @@
  */
   
  get_header();  ?>
+
+
+ <section>
+    <div class="left">
+        <h3>Europe<br>Archive</h3>
+        <div class="item-title"><h4>Three Marias</h4></div>
+        <div class="item-location">Belgium - 1990</div>
+        <div class="item-description">
+            <div class="item-country-logo">
+                <img src="http://europearchive.eu/wp-content/uploads/2020/06/belgium-map-silhouette-vector-14558531.jpg" alt="">
+            </div>
+            <div class="item-description">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
+                exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
+                dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
+                Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
+                anim id est laborum.</p>
+            </div>
+        </div>
+    </div>
+    <div class="right">
+        <img src="http://europearchive.eu/wp-content/uploads/2020/07/three-marias.png" alt="three marias">
+    </div>
+ </section>

f2c14bd109c7842554df7a633b18490e7384da47
Marco <marco@defrostudio.eu>
2020-07-02 15:46:40 +0200

    span + p

--- item.php
+++ item.php
@@ -15,12 +15,12 @@
         <div class="item-description">
             <div class="item-description">
                 <img src="http://europearchive.eu/wp-content/uploads/2020/06/belgium-map-silhouette-vector-14558531.jpg" alt="">
-                <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
+                <span><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor 
                 incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud 
                 exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure 
                 dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
                 Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit 
-                anim id est laborum.</span>
+                anim id est laborum.</p></span>
             </div>
         </div>
     </div>

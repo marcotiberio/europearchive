3b99970494086c8ca3ee8f614f5d37f32e94d003
Marco <marco@defrostudio.eu>
2020-07-11 18:17:44 +0200

    fix

--- home.php
+++ home.php
@@ -5,7 +5,7 @@
 <?php get_header(); ?>
 
 <section class="container">
-    <h1 onclick="window.location.href='http://europearchive.eu/german-shepard-bottle/">Europe<br>Archive</h1>
+    <h1 onclick="window.location.href='http://europearchive.eu/german-shepard-bottle/'">Europe<br>Archive</h1>
     <h2>Erik Kessels / Thomas Mailaender</h2>
 </section>
 <div class="box-bottom">

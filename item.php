286a4798e36b3669c0b883f7a6cc336b626277a7
Marco <marco@defrostudio.eu>
2020-07-11 17:06:08 +0200

    mobile framework

--- item.php
+++ item.php
@@ -43,3 +43,41 @@
         </div>
     </div>
  </section>
+
+ <div class="mobile">
+    <div class="top">
+        <div class="title">
+            <h1>Europe<br>Archive</h1>
+        </div>
+        <div class="img-container">
+            <img id="cover" src="http://europearchive.eu/wp-content/uploads/2020/07/pstoretedesco-front-e1594478203166.png" alt="">
+        </div>
+    </div>
+    <div class="bottom">
+        <div class="item-title"><h2>Three Marias</h2></div>
+        <div class="item-location">
+            <span class="country">Belgium - 1990</span>
+            <span class="code">EA/FR/0001</span>
+        </div>
+        <div class="item-description">
+            <div class="item-description">
+                <img src="http://europearchive.eu/wp-content/uploads/2020/06/belgium-map-silhouette-vector-14558531.jpg" alt="">
+                Lorem ipsum dolor sit amet, consecte
+                tur adipiscing elit. Quisque sit amet
+                ac odio dapibus dictum eu at ipsum.
+                Duis quis tellus porta, egestas sem sed, volutpat
+                leo. Cras posuere consequat imperdiet. Suspendisse
+                sed lorem turpis. Pellentesque sagittis
+                at felis eget facilisis. Orci varius natoque penatibus
+                et magnis dis parturient montes, nascetur
+                ridiculus mus. Donec vitae eleifend orci. Quisque
+                leo lacus, varius vitae luctus at, molestie sed
+                velit. Curabitur orci lectus, consectetur et urna
+                nec, ultricies mollis elit. Donec condimentum leo
+                et consequat ultricies. Ut ultrices pellentesque
+                velit sed blandit. Proin sapien sapien, mollis
+                vitae ex id, faucibus pellentesque lectus.
+            </div>
+        </div>
+    </div>
+ </div>

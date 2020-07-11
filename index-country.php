9c2d9c89f24520851d6dab86fd5b35121af9ce1c
Marco <marco@defrostudio.eu>
2020-07-11 17:49:35 +0200

    index-country.php

---
+++ index-country.php
@@ -0,0 +1,15 @@
+<?php
+/*
+ * Template Name: Index Country
+ */
+  
+ get_header();  ?>
+
+
+ <section class="index-country">
+    <div class="top">
+        <h1>Europe<br>Archive</h1>
+    </div>
+    <div class="bottom">
+    </div>
+ </section>

71c793f08688c083b859f3fc7bdbe3b076e20822
Marco <marco@defrostudio.eu>
2020-07-11 18:11:13 +0200

    grid country to be finished

--- index-country.php
+++ index-country.php
@@ -11,9 +11,9 @@
         <h1>Europe<br>Archive</h1>
     </div>
     <div class="bottom" id="gridCountries">
-        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/netherlands-holland-solid-black-silhouette-map-vector-21773731.jpg" alt=""></div>
-        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/netherlands-holland-solid-black-silhouette-map-vector-21773731.jpg" alt=""></div>
-        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/netherlands-holland-solid-black-silhouette-map-vector-21773731.jpg" alt=""></div>
+        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/austria-silhouette-map-vector-27520355.jpg" alt=""></div>
+        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/belgium-map-silhouette-vector-14558531.jpg" alt=""></div>
+        <div><img src="http://europearchive.eu/wp-content/uploads/2020/06/republic-of-bulgaria-map-silhouette-vector-10864126.jpg" alt=""></div>
         <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/netherlands-holland-solid-black-silhouette-map-vector-21773731.jpg" alt=""></div>
         <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/netherlands-holland-solid-black-silhouette-map-vector-21773731.jpg" alt=""></div>
         <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/netherlands-holland-solid-black-silhouette-map-vector-21773731.jpg" alt=""></div>

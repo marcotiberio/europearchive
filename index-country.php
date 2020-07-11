53c61ca83392be3adaaf37fa9e098a23dc8a5d17
Marco <marco@defrostudio.eu>
2020-07-11 17:52:46 +0200

    grid countries

--- index-country.php
+++ index-country.php
@@ -6,10 +6,38 @@
  get_header();  ?>
 
 
- <section class="index-country">
+ <section class="index-countries">
     <div class="top">
         <h1>Europe<br>Archive</h1>
     </div>
-    <div class="bottom">
+    <div class="bottom" id="gridCountries">
+        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/netherlands-holland-solid-black-silhouette-map-vector-21773731.jpg" alt=""></div>
+        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/netherlands-holland-solid-black-silhouette-map-vector-21773731.jpg" alt=""></div>
+        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/netherlands-holland-solid-black-silhouette-map-vector-21773731.jpg" alt=""></div>
+        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/netherlands-holland-solid-black-silhouette-map-vector-21773731.jpg" alt=""></div>
+        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/netherlands-holland-solid-black-silhouette-map-vector-21773731.jpg" alt=""></div>
+        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/netherlands-holland-solid-black-silhouette-map-vector-21773731.jpg" alt=""></div>
+        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/netherlands-holland-solid-black-silhouette-map-vector-21773731.jpg" alt=""></div>
+        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/netherlands-holland-solid-black-silhouette-map-vector-21773731.jpg" alt=""></div>
+        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/netherlands-holland-solid-black-silhouette-map-vector-21773731.jpg" alt=""></div>
+        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/netherlands-holland-solid-black-silhouette-map-vector-21773731.jpg" alt=""></div>
+        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/netherlands-holland-solid-black-silhouette-map-vector-21773731.jpg" alt=""></div>
+        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/netherlands-holland-solid-black-silhouette-map-vector-21773731.jpg" alt=""></div>
+        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/netherlands-holland-solid-black-silhouette-map-vector-21773731.jpg" alt=""></div>
+        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/netherlands-holland-solid-black-silhouette-map-vector-21773731.jpg" alt=""></div>
+        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/netherlands-holland-solid-black-silhouette-map-vector-21773731.jpg" alt=""></div>
+        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/netherlands-holland-solid-black-silhouette-map-vector-21773731.jpg" alt=""></div>
+        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/netherlands-holland-solid-black-silhouette-map-vector-21773731.jpg" alt=""></div>
+        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/netherlands-holland-solid-black-silhouette-map-vector-21773731.jpg" alt=""></div>
+        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/netherlands-holland-solid-black-silhouette-map-vector-21773731.jpg" alt=""></div>
+        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/netherlands-holland-solid-black-silhouette-map-vector-21773731.jpg" alt=""></div>
+        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/netherlands-holland-solid-black-silhouette-map-vector-21773731.jpg" alt=""></div>
+        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/netherlands-holland-solid-black-silhouette-map-vector-21773731.jpg" alt=""></div>
+        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/netherlands-holland-solid-black-silhouette-map-vector-21773731.jpg" alt=""></div>
+        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/netherlands-holland-solid-black-silhouette-map-vector-21773731.jpg" alt=""></div>
+        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/netherlands-holland-solid-black-silhouette-map-vector-21773731.jpg" alt=""></div>
+        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/netherlands-holland-solid-black-silhouette-map-vector-21773731.jpg" alt=""></div>
+        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/netherlands-holland-solid-black-silhouette-map-vector-21773731.jpg" alt=""></div>
+        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/netherlands-holland-solid-black-silhouette-map-vector-21773731.jpg" alt=""></div>
     </div>
  </section>

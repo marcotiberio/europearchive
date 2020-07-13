143335cdf344e9965769362ad91bd2e41fb3146f
Marco <marco@defrostudio.eu>
2020-07-13 10:48:13 +0200

    test separator

--- item.php
+++ item.php
@@ -14,24 +14,51 @@
             <span class="country">Germany - 1980</span>
             <span class="code">EA/FR/0001</span>
         </div>
+            <svg id="separator" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="399.771" height="34.102" viewBox="0 0 399.771 34.102">
+            <defs>
+                <clipPath id="clip-path">
+                <path id="Path_3" data-name="Path 3" d="M0,0H399.771V-34.1H0Z" fill="none"/>
+                </clipPath>
+            </defs>
+            <g id="Group_6" data-name="Group 6" transform="translate(0 34.102)">
+                <g id="Group_3" data-name="Group 3" clip-path="url(#clip-path)">
+                <g id="Group_1" data-name="Group 1" transform="translate(0 -17.051)">
+                    <path id="Path_1" data-name="Path 1" d="M0,0H348.661" fill="none" stroke="#2b2e34" stroke-width="2"/>
+                </g>
+                <g id="Group_2" data-name="Group 2" transform="translate(382.72 -1)">
+                    <path id="Path_2" data-name="Path 2" d="M0,0A16.051,16.051,0,0,0,16.051-16.051,16.051,16.051,0,0,0,0-32.1,16.051,16.051,0,0,0-16.051-16.051,16.051,16.051,0,0,0,0,0Z" fill="none" stroke="#2b2e34" stroke-width="2"/>
+                </g>
+                </g>
+                <g id="Group_4" data-name="Group 4" transform="translate(373.082 -17.051)">
+                <path id="Path_4" data-name="Path 4" d="M0,0H19.276" fill="none" stroke="#2b2e34" stroke-width="2"/>
+                </g>
+                <g id="Group_5" data-name="Group 5" transform="translate(382.72 -7.413)">
+                <path id="Path_5" data-name="Path 5" d="M0,0V-19.276" fill="none" stroke="#2b2e34" stroke-width="2"/>
+                </g>
+            </g>
+            </svg>
         <div class="item-description">
             <div class="item-description">
                 <img src="http://europearchive.eu/wp-content/uploads/2020/07/germany-silhouette-map-vector-27494653.jpg" alt="">
                 Lorem ipsum dolor sit amet, consecte
-                        tur adipiscing elit. Quisque sit amet
-                        ac odio dapibus dictum eu at ipsum.
-                        Duis quis tellus porta, egestas sem sed, volutpat
-                        leo. Cras posuere consequat imperdiet. Suspendisse
-                        sed lorem turpis. Pellentesque sagittis
-                        at felis eget facilisis. Orci varius natoque penatibus
-                        et magnis dis parturient montes, nascetur
-                        ridiculus mus. Donec vitae eleifend orci. Quisque
-                        leo lacus, varius vitae luctus at, molestie sed
-                        velit. Curabitur orci lectus, consectetur et urna
-                        nec, ultricies mollis elit. Donec condimentum leo
-                        et consequat ultricies. Ut ultrices pellentesque
-                        velit sed blandit. Proin sapien sapien, mollis
-                        vitae ex id, faucibus pellentesque lectus.
+                tur adipiscing elit. Quisque sit amet
+                ac odio dapibus dictum eu at ipsum.
+                Duis quis tellus porta, egestas sem sed, volutpat
+                leo. Cras posuere consequat imperdiet. Suspendisse
+                sed lorem turpis. Pellentesque sagittis
+                at felis eget facilisis. Orci varius natoque penatibus
+                et magnis dis parturient montes, nascetur
+                ridiculus mus. Donec vitae eleifend orci. Quisque
+                leo lacus, varius vitae luctus at, molestie sed
+                velit. Curabitur orci lectus, consectetur et urna
+                nec, ultricies mollis elit. Donec condimentum leo
+                et consequat ultricies. Ut ultrices pellentesque
+                velit sed blandit. Proin sapien sapien, mollis
+                vitae ex id, faucibus pellentesque lectus. Ut ultrices 
+                pellentesque velit sed blandit. Proin sapien sapien, mollis
+                vitae ex id, faucibus pellentesque lectus. Ut ultrices 
+                pellentesque velit sed blandit. Proin sapien sapien, mollis
+                vitae ex id, faucibus pellentesque lectus.
             </div>
         </div>
     </div>

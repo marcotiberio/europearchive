32f75edbe26db442c30162246c9769e2acc0443c
Marco <marco@defrostudio.eu>
2020-07-13 11:19:46 +0200

    separator mobile

--- item.php
+++ item.php
@@ -97,6 +97,20 @@
                 </g>
             </g>
         </svg>
+        <svg id="separatorMobile" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="348.661" height="2" viewBox="0 0 348.661 2">
+            <defs>
+                <clipPath id="clip-path">
+                <path id="Path_11" data-name="Path 11" d="M0,0H348.661V-2H0Z" fill="none"/>
+                </clipPath>
+            </defs>
+            <g id="Group_14" data-name="Group 14" transform="translate(0 2)">
+                <g id="Group_13" data-name="Group 13" clip-path="url(#clip-path)">
+                <g id="Group_12" data-name="Group 12" transform="translate(0 -1)">
+                    <path id="Path_10" data-name="Path 10" d="M0,0H348.661" fill="none" stroke="#2b2e34" stroke-width="2"/>
+                </g>
+                </g>
+            </g>
+        </svg>
         <div class="item-title"><h2>Three Marias</h2></div>
         <div class="item-location">
             <span class="country">Belgium - 1990</span>

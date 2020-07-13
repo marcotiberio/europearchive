ff00412cd0626d65434dbe548c071e933124023c
Marco <marco@defrostudio.eu>
2020-07-13 11:35:26 +0200

    separator desktop

--- item.php
+++ item.php
@@ -9,29 +9,45 @@
 
  <section>
 
- <svg id="separator" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="399.771" height="34.102" viewBox="0 0 399.771 34.102">
+    <div class="separator">
+        <svg id="separatorDesktop" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="611.746" height="2" viewBox="0 0 611.746 2">
             <defs>
                 <clipPath id="clip-path">
-                <path id="Path_3" data-name="Path 3" d="M0,0H399.771V-34.1H0Z" fill="none"/>
+                <path id="Path_11" data-name="Path 11" d="M0,0H611.746V-2H0Z" transform="translate(0 2)" fill="none"/>
                 </clipPath>
             </defs>
-            <g id="Group_6" data-name="Group 6" transform="translate(0 34.102)">
-                <g id="Group_3" data-name="Group 3" clip-path="url(#clip-path)">
-                <g id="Group_1" data-name="Group 1" transform="translate(0 -17.051)">
-                    <path id="Path_1" data-name="Path 1" d="M0,0H348.661" fill="none" stroke="#2b2e34" stroke-width="2"/>
+            <g id="Group_14" data-name="Group 14" transform="translate(0 2)">
+                <g id="Group_13" data-name="Group 13" transform="translate(0 -2)" clip-path="url(#clip-path)">
+                <g id="Group_12" data-name="Group 12" transform="translate(0 1)">
+                    <path id="Path_10" data-name="Path 10" d="M0,0H611.746" fill="none" stroke="#2b2e34" stroke-width="2"/>
                 </g>
-                <g id="Group_2" data-name="Group 2" transform="translate(382.72 -1)">
-                    <path id="Path_2" data-name="Path 2" d="M0,0A16.051,16.051,0,0,0,16.051-16.051,16.051,16.051,0,0,0,0-32.1,16.051,16.051,0,0,0-16.051-16.051,16.051,16.051,0,0,0,0,0Z" fill="none" stroke="#2b2e34" stroke-width="2"/>
-                </g>
-                </g>
-                <g id="Group_4" data-name="Group 4" transform="translate(373.082 -17.051)">
-                <path id="Path_4" data-name="Path 4" d="M0,0H19.276" fill="none" stroke="#2b2e34" stroke-width="2"/>
-                </g>
-                <g id="Group_5" data-name="Group 5" transform="translate(382.72 -7.413)">
-                <path id="Path_5" data-name="Path 5" d="M0,0V-19.276" fill="none" stroke="#2b2e34" stroke-width="2"/>
                 </g>
             </g>
         </svg>
+        <svg id="target" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="34.102" height="34.102" viewBox="0 0 34.102 34.102">
+            <defs>
+                <clipPath id="clip-path">
+                <path id="Path_7" data-name="Path 7" d="M0,0H34.1V-34.1H0Z" fill="none"/>
+                </clipPath>
+            </defs>
+            <g id="Group_11" data-name="Group 11" transform="translate(0 34.102)">
+                <g id="Group_8" data-name="Group 8" clip-path="url(#clip-path)">
+                <g id="Group_7" data-name="Group 7" transform="translate(17.051 -1)">
+                    <path id="Path_6" data-name="Path 6" d="M0,0A16.051,16.051,0,0,0,16.051-16.051,16.051,16.051,0,0,0,0-32.1,16.051,16.051,0,0,0-16.051-16.051,16.051,16.051,0,0,0,0,0Z" fill="none" stroke="#2b2e34" stroke-width="2"/>
+                </g>
+                </g>
+                <g id="Group_9" data-name="Group 9" transform="translate(7.413 -17.051)">
+                <path id="Path_8" data-name="Path 8" d="M0,0H19.276" fill="none" stroke="#2b2e34" stroke-width="2"/>
+                </g>
+                <g id="Group_10" data-name="Group 10" transform="translate(17.051 -7.413)">
+                <path id="Path_9" data-name="Path 9" d="M0,0V-19.276" fill="none" stroke="#2b2e34" stroke-width="2"/>
+                </g>
+            </g>
+        </svg>
+
+
+    </div>
+ 
     <div class="left">
         
         <div class="item-title"><h2>Dog on a bottle that looks like a tree</h2></div>

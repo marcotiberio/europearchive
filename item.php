d5824d43ea2bcf1929f7fd9e78bb10f762f209ef
Marco <marco@defrostudio.eu>
2020-07-13 14:23:46 +0200

    acf fields

--- item.php
+++ item.php
@@ -36,31 +36,15 @@
         </svg>
 
         
-        <div class="item-title"><h2>Dog on a bottle that looks like a tree</h2></div>
+        <div class="item-title"><h2><?php the_field('item_title'); ?></h2></div>
         <div class="item-location">
-            <span class="country">Germany - 1980</span>
-            <span class="code">EA/GE/0001</span>
+            <span class="country"><?php the_field('item_country-year'); ?></span>
+            <span class="code"><?php the_field('item_code'); ?></span>
         </div>
         <div class="item-description">
             <div class="item-description">
                 <img src="http://europearchive.eu/wp-content/uploads/2020/07/germany-silhouette-map-vector-27494653.jpg" alt="">
-                Lorem ipsum dolor sit amet, consecte
-                tur adipiscing elit. Quisque sit amet
-                ac odio dapibus dictum eu at ipsum.
-                Duis quis tellus porta, egestas sem sed, volutpat
-                leo. Cras posuere consequat imperdiet. Suspendisse
-                sed lorem turpis. Pellentesque sagittis
-                at felis eget facilisis. Orci varius natoque penatibus
-                et magnis dis parturient montes, nascetur
-                ridiculus mus. Donec vitae eleifend orci. Quisque
-                leo lacus, varius vitae luctus at, molestie sed
-                velit. Curabitur orci lectus, consectetur et urna
-                nec, ultricies mollis elit. Donec condimentum leo
-                et consequat ultricies. Ut ultrices pellentesque
-                velit sed blandit. Proin sapien sapien, mollis
-                vitae ex id, faucibus pellentesque lectus. Ut ultrices 
-                pellentesque velit sed blandit. Proin sapien sapien, mollis
-                vitae ex id, faucibus pellentesque lectus.
+                <?php the_field('item_description'); ?>
             </div>
         </div>
     </div>
@@ -80,26 +64,6 @@
     </div>
     <div class="bottom">
         <img id="target" src="http://europearchive.eu/wp-content/uploads/2020/07/target@2x.png" alt="">
-        <!-- <svg id="target" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="34.102" height="34.102" viewBox="0 0 34.102 34.102">
-            <defs>
-                <clipPath id="clip-path">
-                <path id="Path_7" data-name="Path 7" d="M0,0H34.1V-34.1H0Z" fill="none"/>
-                </clipPath>
-            </defs>
-            <g id="Group_11" data-name="Group 11" transform="translate(0 34.102)">
-                <g id="Group_8" data-name="Group 8" clip-path="url(#clip-path)">
-                <g id="Group_7" data-name="Group 7" transform="translate(17.051 -1)">
-                    <path id="Path_6" data-name="Path 6" d="M0,0A16.051,16.051,0,0,0,16.051-16.051,16.051,16.051,0,0,0,0-32.1,16.051,16.051,0,0,0-16.051-16.051,16.051,16.051,0,0,0,0,0Z" fill="none" stroke="#2b2e34" stroke-width="2"/>
-                </g>
-                </g>
-                <g id="Group_9" data-name="Group 9" transform="translate(7.413 -17.051)">
-                <path id="Path_8" data-name="Path 8" d="M0,0H19.276" fill="none" stroke="#2b2e34" stroke-width="2"/>
-                </g>
-                <g id="Group_10" data-name="Group 10" transform="translate(17.051 -7.413)">
-                <path id="Path_9" data-name="Path 9" d="M0,0V-19.276" fill="none" stroke="#2b2e34" stroke-width="2"/>
-                </g>
-            </g>
-        </svg> -->
         <svg id="separatorMobile" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="100%" height="2" viewBox="0 0 348.661 2">
             <defs>
                 <clipPath id="clip-path">
@@ -114,31 +78,15 @@
                 </g>
             </g>
         </svg>
-        <div class="item-title"><h2>Dog on a bottle that looks like a tree</h2></div>
+        <div class="item-title"><h2><?php the_field('item_title'); ?></h2></div>
         <div class="item-location">
-            <span class="country">Germany - 1980</span>
-            <span class="code">EA/GE/0001</span>
+            <span class="country"><?php the_field('item_country-year'); ?></span>
+            <span class="code"><?php the_field('item_code'); ?></span>
         </div>
         <div class="item-description">
             <div class="item-description">
                 <img src="http://europearchive.eu/wp-content/uploads/2020/06/belgium-map-silhouette-vector-14558531.jpg" alt="">
-                Lorem ipsum dolor sit amet, consecte
-                tur adipiscing elit. Quisque sit amet
-                ac odio dapibus dictum eu at ipsum.
-                Duis quis tellus porta, egestas sem sed, volutpat
-                leo. Cras posuere consequat imperdiet. Suspendisse
-                sed lorem turpis. Pellentesque sagittis
-                at felis eget facilisis. Orci varius natoque penatibus
-                et magnis dis parturient montes, nascetur
-                ridiculus mus. Donec vitae eleifend orci. Quisque
-                leo lacus, varius vitae luctus at, molestie sed
-                velit. Curabitur orci lectus, consectetur et urna
-                nec, ultricies mollis elit. Donec condimentum leo
-                et consequat ultricies. Ut ultrices pellentesque
-                velit sed blandit. Proin sapien sapien, mollis
-                vitae ex id, faucibus pellentesque lectus. Ut ultrices 
-                pellentesque velit sed blandit. Proin sapien sapien, mollis
-                vitae ex id, faucibus pellentesque lectus.
+                <?php the_field('item_description'); ?>
             </div>
         </div>
     </div>

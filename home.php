51d8928f2c3c861a095cb9eafdc802fbf701de92
Marco <marco@defrostudio.eu>
2020-07-13 10:26:26 +0200

    home logo revert

--- home.php
+++ home.php
@@ -5,10 +5,10 @@
 <?php get_header(); ?>
 
 <section class="container">
-    <svg xmlns="http://www.w3.org/2000/svg" width="567" height="266" viewBox="0 0 567 266">
+    <!-- <svg xmlns="http://www.w3.org/2000/svg" width="1000" height="266" viewBox="0 0 567 266">
         <text id="EUROPE_ARCHIVE" data-name="EUROPE
     ARCHIVE" transform="translate(0 3)" font-size="151.2" font-family="Berthold Akzidenz Grotesk BE"><tspan x="0" y="108">EUROPE</tspan><tspan x="0" y="234">ARCHIVE</tspan></text>
-    </svg>
+    </svg> -->
 
     <h1 onclick="window.location.href='http://europearchive.eu/german-shepard-bottle/'">Europe<br>Archive</h1>
     <h2>Erik Kessels / Thomas Mailaender</h2>

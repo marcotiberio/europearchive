ecb040e9205016de2cf42a0b99e0e31151d89fbb
Marco <marco@defrostudio.eu>
2020-07-13 13:07:06 +0200

    test png separator

--- item.php
+++ item.php
@@ -10,39 +10,7 @@
  <section>
 
     <div class="separator">
-        <svg id="separator" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="593.102" height="34.102" viewBox="0 0 593.102 34.102">
-            <defs>
-                <clipPath id="clip-path">
-                <path id="Path_7" data-name="Path 7" d="M0,0H34.1V-34.1H0Z" fill="none"/>
-                </clipPath>
-                <clipPath id="clip-path-2">
-                <path id="Path_11" data-name="Path 11" d="M0,0H544.2V-2H0Z" transform="translate(0 2)" fill="none"/>
-                </clipPath>
-            </defs>
-            <g id="Group_15" data-name="Group 15" transform="translate(-761 -465.102)">
-                <g id="Group_11" data-name="Group 11" transform="translate(1320 499.204)">
-                <g id="Group_8" data-name="Group 8" clip-path="url(#clip-path)">
-                    <g id="Group_7" data-name="Group 7" transform="translate(17.051 -1)">
-                    <path id="Path_6" data-name="Path 6" d="M0,0A16.051,16.051,0,0,0,16.051-16.051,16.051,16.051,0,0,0,0-32.1,16.051,16.051,0,0,0-16.051-16.051,16.051,16.051,0,0,0,0,0Z" fill="none" stroke="#2b2e34" stroke-width="2"/>
-                    </g>
-                </g>
-                <g id="Group_9" data-name="Group 9" transform="translate(7.413 -17.051)">
-                    <path id="Path_8" data-name="Path 8" d="M0,0H19.276" fill="none" stroke="#2b2e34" stroke-width="2"/>
-                </g>
-                <g id="Group_10" data-name="Group 10" transform="translate(17.051 -7.413)">
-                    <path id="Path_9" data-name="Path 9" d="M0,0V-19.276" fill="none" stroke="#2b2e34" stroke-width="2"/>
-                </g>
-                </g>
-                <g id="Group_14" data-name="Group 14" transform="translate(761 484)">
-                <g id="Group_13" data-name="Group 13" transform="translate(0 -2)" clip-path="url(#clip-path-2)">
-                    <g id="Group_12" data-name="Group 12" transform="translate(0 1)">
-                    <path id="Path_10" data-name="Path 10" d="M0,0H544.2" fill="none" stroke="#2b2e34" stroke-width="2"/>
-                    </g>
-                </g>
-                </g>
-            </g>
-        </svg>
-
+        <img src="assets/separator@3x.png" alt="">
     </div>
  
     <div class="left">

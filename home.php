0d81af9e95b614613404d22c1fb11f31cfa0a258
Marco <marco@defrostudio.eu>
2020-07-14 08:55:44 +0200

    link from home

--- home.php
+++ home.php
@@ -5,9 +5,9 @@
 <?php get_header(); ?>
 
 <section class="container">
-    <img id="logoHome" src="http://europearchive.eu/wp-content/uploads/2020/07/europearchive-logo-home.png" alt="">
-    <h1 onclick="window.location.href='http://europearchive.eu/german-shepard-bottle/'">Europe<br>Archive</h1>
-    <h2>Erik Kessels / Thomas Mailaender</h2>
+    <img id="logoHome" src="http://europearchive.eu/wp-content/uploads/2020/07/europearchive-logo-home.png" alt="" onclick="window.location.href='http://europearchive.eu/german-shepard-bottle/'">
+    <!-- <h1 onclick="window.location.href='http://europearchive.eu/german-shepard-bottle/'">Europe<br>Archive</h1>
+    <h2>Erik Kessels / Thomas Mailaender</h2> -->
 </section>
 <div class="box-bottom">
 </div>

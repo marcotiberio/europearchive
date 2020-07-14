4b88e2bcd4ededafaefdaa3d61db56980802dd8b
Marco <marco@defrostudio.eu>
2020-07-14 09:04:37 +0200

    logotitle png

--- header.php
+++ header.php
@@ -26,11 +26,12 @@
 <body <?php body_class(); ?>>
 <?php wp_body_open(); ?>
 <div id="page" class="site">
-	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'europe-archive' ); ?></a>
+	<!-- <a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'europe-archive' ); ?></a> -->
 
 	<header id="masthead" class="site-header">
 		<div class="site-branding">
-			<h1>Europe<br>Archive</h1>
+			<img id="logoTitle" src="http://europearchive.eu/wp-content/uploads/2020/07/europearchive-logo-title.png" alt="" onclick="window.location.href='http://europearchive.eu'">
+			<!-- <h1>Europe<br>Archive</h1> -->
 		</div><!-- .site-branding -->
 
 		<nav id="site-navigation" class="main-navigation">

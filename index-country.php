61bc647899b757a7ef3ccaa37cab739c25f6980d
Marco <marco@defrostudio.eu>
2020-07-25 00:03:02 +0200

    france, hungary, bulgaria

--- index-country.php
+++ index-country.php
@@ -10,17 +10,17 @@
     <div class="bottom" id="gridCountries">
         <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/austria-silhouette-map-vector-27520355.jpg" alt=""></div>
         <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/belgium-map-silhouette-vector-14558531.jpg" alt=""></div>
-        <div><img src="http://europearchive.eu/wp-content/uploads/2020/06/republic-of-bulgaria-map-silhouette-vector-10864126.jpg" alt=""></div>
+        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/bulgaria.jpg" alt=""></div>
         <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/croatia-map-silhouette-isolated-vector-28801394.jpg" alt=""></div>
         <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/cyprus-vector-13690.jpg" alt=""></div>
         <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/czech-republic-silhouette-map-vector-27520357.jpg" alt=""></div>
         <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/denmark-solid-black-silhouette-map-of-country-vector-21714778.jpg" alt=""></div>
         <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/estonia-silhouette-map-vector-28115175.jpg" alt=""></div>
         <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/finland-solid-black-silhouette-map-of-country-vector-21714780.jpg" alt=""></div>
-        <div><img src="http://europearchive.eu/wp-content/uploads/2020/06/france-map-silhouette-french-map-isolated-vector-28801462.jpg" alt=""></div>
+        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/france.jpg" alt=""></div>
         <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/germany-silhouette-map-vector-27494653.jpg" alt=""></div>
         <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/greece-solid-black-silhouette-map-of-country-vector-21665532.jpg" alt=""></div>
-        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/hungary-map-silhouette-isolated-on-white-vector-28790408.jpg" alt=""></div>
+        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/hungary.jpg" alt=""></div>
         <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/ireland.jpg" alt=""></div>
         <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/italy-silhouette-map-vector-27482741.jpg" alt=""></div>
         <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/latvia-silhouette-map-vector-28101934.jpg" alt=""></div>

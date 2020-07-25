6f08664ea91b3a5657a7c1f1d3ebc181c1b019c8
Marco <marco@defrostudio.eu>
2020-07-25 20:47:13 +0200

    test link blank

--- index-country.php
+++ index-country.php
@@ -10,7 +10,7 @@
     <div class="bottom" id="gridCountries">
         <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/austria-silhouette-map-vector-27520355.jpg" alt=""></div>
         <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/belgium-map-silhouette-vector-14558531.jpg" alt=""></div>
-        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/bulgaria.jpg" alt=""></div>
+        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/bulgaria.jpg" alt="" onclick="window.open('http://europearchive.eu/german-shepard-bottle', '_blank');"></div>
         <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/croatia-map-silhouette-isolated-vector-28801394.jpg" alt=""></div>
         <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/cyprus-vector-13690.jpg" alt=""></div>
         <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/czech-republic-silhouette-map-vector-27520357.jpg" alt=""></div>
@@ -18,22 +18,22 @@
         <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/estonia-silhouette-map-vector-28115175.jpg" alt=""></div>
         <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/finland-solid-black-silhouette-map-of-country-vector-21714780.jpg" alt=""></div>
         <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/france.jpg" alt=""></div>
-        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/germany-silhouette-map-vector-27494653.jpg" alt="" onclick="window.location.href='http://europearchive.eu/german-shepard-bottle'"></div>
+        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/germany-silhouette-map-vector-27494653.jpg" alt="" onclick="window.open('http://europearchive.eu/german-shepard-bottle', '_blank');"></div>
         <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/greece-solid-black-silhouette-map-of-country-vector-21665532.jpg" alt=""></div>
         <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/hungary.jpg" alt=""></div>
         <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/ireland.jpg" alt=""></div>
-        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/italy-silhouette-map-vector-27482741.jpg" alt=""></div>
+        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/italy-silhouette-map-vector-27482741.jpg" alt="" onclick="window.open('http://europearchive.eu/german-shepard-bottle', '_blank');"></div>
         <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/latvia-silhouette-map-vector-28101934.jpg" alt=""></div>
         <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/lithuania-silhouette-map-vector-28101935.jpg" alt=""></div>
         <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/luxembourg-silhouette-map-vector-27520359.jpg" alt=""></div>
-        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/netherlands-holland-solid-black-silhouette-map-vector-21773731.jpg" alt=""></div>
+        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/netherlands-holland-solid-black-silhouette-map-vector-21773731.jpg" alt="" onclick="window.open('https://open-house.architektur.tu-berlin.de/vr/index.html', '_blank');"></div>
         <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/malta-map-silhouette-isolated-on-white-vector-28790549.jpg" alt=""></div>
         <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/poland-silhouette-map-vector-27594581.jpg" alt=""></div>
         <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/portugal-solid-black-silhouette-map-of-country-vector-21714879.jpg" alt=""></div>
         <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/romania-silhouette-map-vector-27674836.jpg" alt=""></div>
         <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/slovakia-silhouette-map-vector-27803739.jpg" alt=""></div>
         <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/black-silhouette-country-borders-map-of-slovenia-vector-19256111.jpg" alt=""></div>
-        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/spain-silhouette-map-vector-26961823.jpg" alt=""></div>
+        <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/spain-silhouette-map-vector-26961823.jpg" alt="" onclick="window.open('http://europearchive.eu/german-shepard-bottle', '_blank');"></div>
         <div><img src="http://europearchive.eu/wp-content/uploads/2020/07/sweden-silhouette-map-vector-28202560.jpg" alt=""></div>
     </div>
  </section>

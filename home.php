031733c3587e6e0280a8fd65ce91a41df5ba5e13
Marco <marco@defrostudio.eu>
2020-07-25 23:41:00 +0200

    fix logo link

--- home.php
+++ home.php
@@ -5,7 +5,7 @@
 <?php get_header(); ?>
 
 <section class="container">
-    <img id="logoHome" src="http://europearchive.eu/wp-content/uploads/2020/07/europearchive-logo-home.png" alt="" onclick="window.location.href='http://europearchive.eu/german-shepard-bottle/'">
+    <img id="logoHome" src="http://europearchive.eu/wp-content/uploads/2020/07/europearchive-logo-home.png" alt="">
     <!-- <h1 onclick="window.location.href='http://europearchive.eu/german-shepard-bottle/'">Europe<br>Archive</h1>
     <h2>Erik Kessels / Thomas Mailaender</h2> -->
 </section>

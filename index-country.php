2a7847055865aa9e28424940fc8745fab760c8c7
Marco <marco@defrostudio.eu>
2020-07-30 22:49:49 +0200

    last countries items

--- index-country.php
+++ index-country.php
@@ -12,27 +12,27 @@
         <div id="iconBelgium" onclick="window.open('https://europearchive.eu/pan-sculpture-looking-like-a-house/', '_blank');"></div>
         <div id="iconBulgaria" onclick="window.open('https://europearchive.eu/birth-and-death-photograph-of-the-same-person/', '_blank');"></div>
         <div id="iconCroatia" onclick="window.open('https://europearchive.eu/test-panels-for-gravestone-plaques/', '_blank');"></div>
-        <div id="iconCyprus"></div>
+        <div id="iconCyprus" onclick="window.open('https://europearchive.eu/item-cyprus/', '_blank');"></div>
         <div id="iconCzech" onclick="window.open('https://europearchive.eu/lamp-made-out-of-a-deers-leg/', '_blank');"></div>
         <div id="iconDenmark" onclick="window.open('https://europearchive.eu/cross-over-between-a-hat-and-a-cap/', '_blank');"></div>
         <div id="iconEstonia" onclick="window.open('https://europearchive.eu/2-taxidermy-frogs-holding-an-ashtray/', '_blank');"></div>
-        <div id="iconFinland"></div>
+        <div id="iconFinland" onclick="window.open('https://europearchive.eu/item-finland/', '_blank');"></div>
         <div id="iconFrance" onclick="window.open('https://europearchive.eu/shame-medal-for-13-0-loosing-at-petanque/', '_blank');"></div>
         <div id="iconGermany" onclick="window.open('https://europearchive.eu/wwii-german-bunker-ashtray/', '_blank');"></div>
         <div id="iconGreece" onclick="window.open('https://europearchive.eu/shell-sculpture-with-two-funny-creatures/', '_blank');"></div>
         <div id="iconHungary" onclick="window.open('https://europearchive.eu/metal-beware-the-dog-sign/', '_blank');"></div>
         <div id="iconIreland" onclick="window.open('https://europearchive.eu/9-instruction-photos-for-wearing-diver-suit/', '_blank');"></div>
         <div id="iconItaly" onclick="window.open('https://europearchive.eu/handmade-display-of-a-spoon-and-fork-salesman/', '_blank');"></div>
-        <div id="iconLatvia"></div>
+        <div id="iconLatvia" onclick="window.open('https://europearchive.eu/item-latvia/', '_blank');"></div>
         <div id="iconLithuania" onclick="window.open('https://europearchive.eu/female-shoes-stuffed-with-newspapers/', '_blank');"></div>
-        <div id="iconLuxembourg"></div>
+        <div id="iconLuxembourg" onclick="window.open('https://europearchive.eu/item-luxembourg/', '_blank');"></div>
         <div id="iconNetherlands" onclick="window.open('https://europearchive.eu/photos-of-woman-trying-to-jump-of-roof/', '_blank');"></div>
-        <div id="iconMalta"></div>
+        <div id="iconMalta" onclick="window.open('https://europearchive.eu/item-malta/', '_blank');"></div>
         <div id="iconPoland" onclick="window.open('https://europearchive.eu/folklore-dancers-on-vinyl-postcards/', '_blank');"></div>
         <div id="iconPortugal" onclick="window.open('https://europearchive.eu/pocket-passport-photo-album-of-3-brothers/', '_blank');"></div>
         <div id="iconRomania" onclick="window.open('https://europearchive.eu/hand-made-frame-made-out-of-thread/', '_blank');"></div>
-        <div id="iconSlovakia"></div>
-        <div id="iconSlovenia"></div>
+        <div id="iconSlovakia" onclick="window.open('https://europearchive.eu/item-slovakia/', '_blank');"></div>
+        <div id="iconSlovenia" onclick="window.open('https://europearchive.eu/item-slovenia/', '_blank');"></div>
         <div id="iconSpain" onclick="window.open('https://europearchive.eu/flamenco-dancers-holding-10-matchboxes/', '_blank');"></div>
         <div id="iconSweden"onclick="window.open('https://europearchive.eu/3-wooden-blocks-for-educational-purposes/', '_blank');"></div>
         <!-- <div><img id="iconAustria" src="http://europearchive.eu/wp-content/uploads/2020/07/austria-silhouette-map-vector-27520355.jpg" alt=""></div>

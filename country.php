aed1aea057901067745333dcf9884f47de985038
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-08-26 17:19:31 +0200

    repositioned page title

--- country.php
+++ country.php
@@ -5,25 +5,25 @@
   
  get_header();  ?>
 
-<?php
-    if ( is_singular() ) :
-        the_title( '<h1 class="entry-title">', '</h1>' );
-    else :
-        the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
-    endif;
-
-    if ( 'post' === get_post_type() ) :
-        ?>
-        <div class="entry-meta">
-            <?php
-            europe_archive_posted_on();
-            europe_archive_posted_by();
-            ?>
-        </div><!-- .entry-meta -->
-<?php endif; ?>
-
  <section class="index-countries">
     <div class="bottom" id="gridCountries">
+
+        <?php
+            if ( is_singular() ) :
+                the_title( '<h1 class="entry-title">', '</h1>' );
+            else :
+                the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+            endif;
+
+            if ( 'post' === get_post_type() ) :
+                ?>
+                <div class="entry-meta">
+                    <?php
+                    europe_archive_posted_on();
+                    europe_archive_posted_by();
+                    ?>
+                </div><!-- .entry-meta -->
+        <?php endif; ?>
 
         <?php if( have_rows('icon_item') ): ?>
             <?php while( have_rows('icon_item') ): the_row(); 

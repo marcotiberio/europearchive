d452a03af6c42e4fe699700862f1d7b07455824a
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-08-26 17:18:34 +0200

    acf repeater working test page title

--- country.php
+++ country.php
@@ -5,6 +5,22 @@
   
  get_header();  ?>
 
+<?php
+    if ( is_singular() ) :
+        the_title( '<h1 class="entry-title">', '</h1>' );
+    else :
+        the_title( '<h2 class="entry-title"><a href="' . esc_url( get_permalink() ) . '" rel="bookmark">', '</a></h2>' );
+    endif;
+
+    if ( 'post' === get_post_type() ) :
+        ?>
+        <div class="entry-meta">
+            <?php
+            europe_archive_posted_on();
+            europe_archive_posted_by();
+            ?>
+        </div><!-- .entry-meta -->
+<?php endif; ?>
 
  <section class="index-countries">
     <div class="bottom" id="gridCountries">

b5636dcf1f8af9d3821ad0f7ba54aba7f45a3ead
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-08-26 16:30:46 +0200

    setup country template and acf

---
+++ country.php
@@ -0,0 +1,66 @@
+<?php
+/*
+ * Template Name: Country
+ */
+  
+ get_header();  ?>
+
+
+ <section class="index-countries">
+    <div class="bottom" id="gridCountries">
+        <div id="iconTest" style="background_image: url(<?php the_field('background_image'); ?>);" onclick="window.open('https://europearchive.eu/portfolio-of-a-teacher-with-student-letters/', '_blank');"></div>
+        <div id="iconBelgium" onclick="window.open('https://europearchive.eu/pan-sculpture-looking-like-a-house/', '_blank');"></div>
+        <div id="iconBulgaria" onclick="window.open('https://europearchive.eu/birth-and-death-photo-of-the-same-person/', '_blank');"></div>
+        <div id="iconCroatia" onclick="window.open('https://europearchive.eu/exploded-bombshell-used-as-a-flower-vase/', '_blank');"></div>
+        <div id="iconCyprus" onclick="window.open('https://europearchive.eu/super-8-film-of-people-eatings-lobster/', '_blank');"></div>
+        <div id="iconCzech" onclick="window.open('https://europearchive.eu/lamp-made-out-of-a-deers-leg/', '_blank');"></div>
+        <div id="iconDenmark" onclick="window.open('https://europearchive.eu/cross-over-between-a-hat-and-a-cap/', '_blank');"></div>
+        <div id="iconEstonia" onclick="window.open('https://europearchive.eu/two-taxidermy-frogs-holding-an-ashtray/', '_blank');"></div>
+        <div id="iconFinland" onclick="window.open('https://europearchive.eu/photo-album-of-two-girls-and-bananas/', '_blank');"></div>
+        <div id="iconFrance" onclick="window.open('https://europearchive.eu/shame-medal-for-loosing-at-petanque/', '_blank');"></div>
+        <div id="iconGermany" onclick="window.open('https://europearchive.eu/wwii-german-bunker-ashtray/', '_blank');"></div>
+        <div id="iconGreece" onclick="window.open('https://europearchive.eu/shell-sculpture-with-two-funny-creatures/', '_blank');"></div>
+        <div id="iconHungary" onclick="window.open('https://europearchive.eu/metal-beware-the-dog-sign/', '_blank');"></div>
+        <div id="iconIreland" onclick="window.open('https://europearchive.eu/nine-instruction-photos-for-wearing-diver-suit/', '_blank');"></div>
+        <div id="iconItaly" onclick="window.open('https://europearchive.eu/handmade-display-of-a-cutlery-salesman/', '_blank');"></div>
+        <div id="iconLatvia" onclick="window.open('https://europearchive.eu/eight-erotic-slides-with-a-vagina-and-a-jewel/', '_blank');"></div>
+        <div id="iconLithuania" onclick="window.open('https://europearchive.eu/female-shoes-stuffed-with-newspapers/', '_blank');"></div>
+        <div id="iconLuxembourg" onclick="window.open('https://europearchive.eu/pharmaceutic-packshot-photography-archive/', '_blank');"></div>
+        <div id="iconNetherlands" onclick="window.open('https://europearchive.eu/photos-of-a-woman-trying-to-jump-of-roof/', '_blank');"></div>
+        <div id="iconMalta" onclick="window.open('https://europearchive.eu/faience-plate-with-the-portrait-of-woman/', '_blank');"></div>
+        <div id="iconPoland" onclick="window.open('https://europearchive.eu/folklore-dancers-on-vinyl-postcards/', '_blank');"></div>
+        <div id="iconPortugal" onclick="window.open('https://europearchive.eu/pocket-passport-album-of-three-brothers/', '_blank');"></div>
+        <div id="iconRomania" onclick="window.open('https://europearchive.eu/handmade-frame-made-out-of-thread/', '_blank');"></div>
+        <div id="iconSlovakia" onclick="window.open('https://europearchive.eu/exploded-bombshell-used-as-a-flower-vase/', '_blank');"></div>
+        <div id="iconSlovenia" onclick="window.open('https://europearchive.eu/amateur-painting-of-a-swiss-lake-with-a-couple-of-swans/', '_blank');"></div>
+        <div id="iconSpain" onclick="window.open('https://europearchive.eu/flamenco-dancers-holding-ten-matchboxes/', '_blank');"></div>
+        <div id="iconSweden"onclick="window.open('https://europearchive.eu/three-wooden-blocks-for-educational-purposes/', '_blank');"></div>
+        <!-- <div><img id="iconAustria" src="http://europearchive.eu/wp-content/uploads/2020/07/austria-silhouette-map-vector-27520355.jpg" alt=""></div>
+        <div><img id="iconBelgium" src="http://europearchive.eu/wp-content/uploads/2020/07/belgium-map-silhouette-vector-14558531.jpg" alt=""></div>
+        <div><img id="iconBulgaria" src="http://europearchive.eu/wp-content/uploads/2020/07/bulgaria.jpg" alt="" onclick="window.open('http://europearchive.eu/birth-and-death-photograph-of-the-same-person/', '_blank');"></div>
+        <div><img id="iconCroatia" src="http://europearchive.eu/wp-content/uploads/2020/07/croatia-map-silhouette-isolated-vector-28801394.jpg" alt=""></div>
+        <div><img id="iconCyprus" src="http://europearchive.eu/wp-content/uploads/2020/07/cyprus-vector-13690.jpg" alt=""></div>
+        <div><img id="iconCzech" src="http://europearchive.eu/wp-content/uploads/2020/07/czech-republic-silhouette-map-vector-27520357.jpg" alt=""></div>
+        <div><img id="iconDenmark" src="http://europearchive.eu/wp-content/uploads/2020/07/denmark-solid-black-silhouette-map-of-country-vector-21714778.jpg" alt=""></div>
+        <div><img id="iconEstonia" src="http://europearchive.eu/wp-content/uploads/2020/07/estonia-silhouette-map-vector-28115175.jpg" alt=""></div>
+        <div><img id="iconFinland" src="http://europearchive.eu/wp-content/uploads/2020/07/finland-solid-black-silhouette-map-of-country-vector-21714780.jpg" alt=""></div>
+        <div><img id="iconFrance" src="http://europearchive.eu/wp-content/uploads/2020/07/france.jpg" alt="" onclick="window.open('http://europearchive.eu/les-debuts-de-colette/', '_blank');"></div>
+        <div><img id="iconGermany" src="http://europearchive.eu/wp-content/uploads/2020/07/germany-silhouette-map-vector-27494653.jpg" alt="" onclick="window.open('http://europearchive.eu/german-shepard-bottle', '_blank');"></div>
+        <div><img id="iconGreece" src="http://europearchive.eu/wp-content/uploads/2020/07/greece-solid-black-silhouette-map-of-country-vector-21665532.jpg" alt=""></div>
+        <div><img id="iconHungary" src="http://europearchive.eu/wp-content/uploads/2020/07/hungary.jpg" alt=""></div>
+        <div><img id="iconIreland" src="http://europearchive.eu/wp-content/uploads/2020/07/ireland.jpg" alt=""></div>
+        <div><img id="iconItaly" src="http://europearchive.eu/wp-content/uploads/2020/07/italy-silhouette-map-vector-27482741.jpg" alt="" onclick="window.open('http://europearchive.eu/handmade-display-of-a-spoon-and-fork-salesman/', '_blank');"></div>
+        <div><img id="iconLatvia" src="http://europearchive.eu/wp-content/uploads/2020/07/latvia-silhouette-map-vector-28101934.jpg" alt=""></div>
+        <div><img id="iconLithuania" src="http://europearchive.eu/wp-content/uploads/2020/07/lithuania-silhouette-map-vector-28101935.jpg" alt=""></div>
+        <div><img id="iconLuxembourg" src="http://europearchive.eu/wp-content/uploads/2020/07/luxembourg-silhouette-map-vector-27520359.jpg" alt=""></div>
+        <div><img id="iconNetherlands" src="http://europearchive.eu/wp-content/uploads/2020/07/netherlands-holland-solid-black-silhouette-map-vector-21773731.jpg" alt="" onclick="window.open('http://europearchive.eu/filmslides-in-a-tiny-matchbox/', '_blank');"></div>
+        <div><img id="iconMalta" src="http://europearchive.eu/wp-content/uploads/2020/07/malta-map-silhouette-isolated-on-white-vector-28790549.jpg" alt=""></div>
+        <div><img id="iconPoland" src="http://europearchive.eu/wp-content/uploads/2020/07/poland-silhouette-map-vector-27594581.jpg" alt="" onclick="window.open('http://europearchive.eu/soft-erotic-scrapbook/', '_blank');"></div>
+        <div><img id="iconPortugal" src="http://europearchive.eu/wp-content/uploads/2020/07/portugal-solid-black-silhouette-map-of-country-vector-21714879.jpg" alt=""></div>
+        <div><img id="iconRomania" src="http://europearchive.eu/wp-content/uploads/2020/07/romania-silhouette-map-vector-27674836.jpg" alt=""></div>
+        <div><img id="iconSlovakia" src="http://europearchive.eu/wp-content/uploads/2020/07/slovakia-silhouette-map-vector-27803739.jpg" alt=""></div>
+        <div><img id="iconSlovenia" src="http://europearchive.eu/wp-content/uploads/2020/07/black-silhouette-country-borders-map-of-slovenia-vector-19256111.jpg" alt=""></div>
+        <div><img id="iconSpain" src="http://europearchive.eu/wp-content/uploads/2020/07/spain-silhouette-map-vector-26961823.jpg" alt="" onclick="window.open('http://europearchive.eu/flamenco-dancers-holding-10-matchboxes/', '_blank');"></div>
+        <div><img id="iconSweden" src="http://europearchive.eu/wp-content/uploads/2020/07/sweden-silhouette-map-vector-28202560.jpg" alt=""></div> -->
+    </div>
+ </section>

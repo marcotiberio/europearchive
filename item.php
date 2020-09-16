4b39749cf11e5787315440c96238e8e9c019d30b
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-16 09:19:50 +0200

    full screen target modal

--- item.php
+++ item.php
@@ -14,7 +14,7 @@
  
     <div class="left">
 
-        <svg id="targetDesktop" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="34.102" height="34.102" viewBox="0 0 34.102 34.102">
+        <svg id="targetDesktop" width="34.102" height="34.102" viewBox="0 0 34.102 34.102">
             <defs>
                 <clipPath id="clip-path">
                 <path id="Path_7" data-name="Path 7" d="M0,0H34.1V-34.1H0Z" fill="none"/>
@@ -102,3 +102,50 @@
         </div>
     </div>
  </div>
+
+
+
+
+
+    <!-- Full Screen Modal -->
+    <div id="fullscreenModal" class="modal">
+
+        <!-- Full Screen Modal - Content -->
+        <div class="modal-content">
+            <span class="close">&times;</span>
+            <div class="fullscreen-item">
+                <img src="https://europearchive.eu/wp-content/uploads/2020/09/P7102179_web.jpg" alt="">
+            </div>
+        </div>
+
+    </div>
+
+
+
+<script>
+    // Get the modal
+    var modal = document.getElementById("fullscreenModal");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("targetDesktop");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks the button, open the modal 
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+</script>

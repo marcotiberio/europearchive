e69e9db6882b9fabce6f84fcdb317b63227ce8fd
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-25 14:55:07 +0200

    test

--- item.php
+++ item.php
@@ -51,29 +51,12 @@
                     $url = $image['url'];
                     $title = $image['title'];
                     $alt = $image['alt'];
-                    $caption = $image['caption'];
-
-                    // Thumbnail size attributes.
-                    $size = 'thumbnail';
-                    $thumb = $image['sizes'][ $size ];
-                    $width = $image['sizes'][ $size . '-width' ];
-                    $height = $image['sizes'][ $size . '-height' ];
-
-                    // Begin caption wrap.
-                    if( $caption ): ?>
-                        <div class="wp-caption">
-                    <?php endif; ?>
-
+                    $caption = $image['caption']; ?>
+                        
                     <a href="<?php echo esc_attr($alt); ?>" title="<?php echo esc_attr($title); ?>">
-                        <img src="<?php echo esc_url($thumb); ?>" alt="<?php echo esc_attr($alt); ?>" />
+                        <img src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($alt); ?>" />
                     </a>
 
-                    <?php 
-                    // End caption wrap.
-                    if( $caption ): ?>
-                        <p class="wp-caption-text"><?php echo esc_html($caption); ?></p>
-                        </div>
-                    <?php endif; ?>
                 <?php endif; ?>
                 <?php the_field('item_description'); ?>
             </div>

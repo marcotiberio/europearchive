fc23db9f1e6ff83f6df82796335dde49f4687873
Marco Tiberio <45846191+marcotiberio@users.noreply.github.com>
2020-09-25 14:43:09 +0200

    revert

--- item.php
+++ item.php
@@ -62,38 +62,11 @@
  <div class="mobile">
     <div class="top">
         <div class="img-container">
-        <?php
+        <?php 
             $image = get_field('item_cover_image');
-            if( $image ):
-
-                // Image variables.
-                $url = $image['url'];
-                $title = $image['title'];
-                $alt = $image['alt'];
-                $caption = $image['caption'];
-
-                // Thumbnail size attributes.
-                $size = 'thumbnail';
-                $thumb = $image['sizes'][ $size ];
-                $width = $image['sizes'][ $size . '-width' ];
-                $height = $image['sizes'][ $size . '-height' ];
-
-                // Begin caption wrap.
-                if( $caption ): ?>
-                    <div class="wp-caption">
-                <?php endif; ?>
-
-                <a href="<?php echo esc_url($url); ?>" title="<?php echo esc_attr($title); ?>">
-                    <img id="cover" src="<?php echo esc_url($thumb); ?>" alt="<?php echo esc_attr($alt); ?>" />
-                </a>
-
-                <?php 
-                // End caption wrap.
-                if( $caption ): ?>
-                    <p class="wp-caption-text"><?php echo esc_html($caption); ?></p>
-                    </div>
-                <?php endif; ?>
-            <?php endif; ?>
+            if( !empty( $image ) ): ?>
+                <img id="cover" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
+        <?php endif; ?>
         </div>
     </div>
     <div class="bottom">
